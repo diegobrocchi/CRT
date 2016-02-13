9800de352b6af0b4897d8e65dcd2dcc11b1c623c
diegobrocchi <dbrocchi@gmail.com>
2016-02-13 17:27:40 +0100

    Initial commit
    Empty project created.

---
+++ CRT/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CRT.Startup))]
+namespace CRT
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
